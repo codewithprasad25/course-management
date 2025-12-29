9b5ac63abb305bb407a682679baed05aee432dd3
Prasad Shinde <prasadshinde1305@gmail.com>
2025-12-29 11:00:15 +0530

    git push

---
+++ course-management/src/main/java/com/example/course_management/Repository/StudentRepo.java
@@ -0,0 +1,9 @@
+package com.example.course_management.Repository;
+
+import com.example.course_management.Models.Student;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepo {
+
+}
